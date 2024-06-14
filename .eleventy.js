5d5d0224dee1f63a5a5ec3a4174634e861b9c8bc
alant <alan.htang@gmail.com>
2024-06-13 22:18:47 -0700

    add sidebar, have a front page

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,18 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const markdownIt = require('markdown-it')
-const markdownItAnchor = require('markdown-it-anchor')
+const markdownItAttrs = require('markdown-it-attrs');
 const pluginTOC = require('eleventy-plugin-toc')
 
+let markdownLib = markdownIt({
+  html: true,
+  breaks: true,
+  linkify: true
+}).use(markdownItAttrs);
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.setLibrary('md', markdownIt().use(markdownItAnchor));
+  eleventyConfig.setLibrary('md', markdownLib);
   eleventyConfig.addPlugin(pluginTOC);
-  eleventyConfig.addPassthroughCopy("assets/css/styles.css"); 
+  eleventyConfig.addPassthroughCopy("assets"); 
 	eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addGlobalData('currentYear', new Date().getFullYear());
 };
